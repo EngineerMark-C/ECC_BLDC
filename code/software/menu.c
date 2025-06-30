4a9f44a65437243bf94c13e184021919bd6583c5
Mark <2831694483@qq.com>
2025-06-30 13:21:57 +0800

    - 添加发车延时

--- code/software/menu.c
+++ code/software/menu.c
@@ -1011,6 +1011,10 @@ void Main_Menu_Key_Process(void)
     }
     if (key5_state == KEY_SHORT_PRESS)
     {
+        if (Fire_Flag == 0)
+        {
+            system_delay_ms(2000);
+        }
         Fire_Flag = Fire_Flag ? 0 : 1;
         key_clear_state(KEY_5);
     }
